7d7f4a94be371a96c689c7f3799620c264e0e9b6
Rory Williams <rlwillia@caltech.edu>
2016-09-13 14:28:25 -0700

    updated gitignore, Tuesday bootcamp afternoon

---
+++ numpy_practice.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+xa_high = np.loadtxt('data/xa_high_food.csv')
+xa_low = np.loadtxt('data/xa_low_food.csv')
+
+def xa_to_diameter(xa):
+
+    """
+    Convert an array of cross-sectional areas to diameters with commensurate
+    """
+
+    # Compute diameter from area
+    # A = pi * d^2 / 4
+    diameter = np.sqrt(xa *4 / np.pi)
+
+    return diameter
+
+A = np.array([[6.7, 1.3, 0.6, 0.7],
+              [0.1, 5.5, 0.4, 2.4],
+              [1.1, 0.8, 4.5, 1.7],
+              [0.0, 1.5, 3.4, 7.5]])
+
+b = np.array([1.1, 2.3, 3.3, 3.9])
