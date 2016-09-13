8aca71c6b92c2f2ec9bc159e223cfbc3be6d336f
Rory Williams <rlwillia@caltech.edu>
2016-09-13 15:51:35 -0700

    Added ecdf function

---
+++ bootcamp_utils.py
@@ -0,0 +1,14 @@
+"""
+Bootcamp utils: A collection of statistical functions
+proved useful during Bootcamp
+"""
+import numpy as np
+
+def ecdf(data):
+    """
+    Compute x, y values for an empirical distribution function
+    """
+    x = np.sort(data)
+    y = np.arange(1, 1+len(x)) / len(x)
+
+    return x, y
