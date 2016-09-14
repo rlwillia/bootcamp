62468a2619bed9695df99783055e89de505b6899
Rory Williams <rlwillia@caltech.edu>
2016-09-14 12:02:19 -0700

    Added boostrap function to bootcamp utils, hacker stats notes

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -12,3 +12,16 @@ def ecdf(data):
     y = np.arange(1, 1+len(x)) / len(x)
 
     return x, y
+
+def bootstrap_dist(data, func = np.mean, n_reps = 100000):
+    """
+    Resamples data n times and computes statistic of interest
+    returns
+    """
+
+    bs_replicates = np.empty(n_reps)
+    for i in range(n_reps):
+        bs_sample = np.random.choice(data, replace=True, size=len(data))
+        bs_replicates[i] = func(bs_sample)
+
+    return bs_replicates
